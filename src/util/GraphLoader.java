211d30ee2e4bab6214e4b558d4fe9254915bd8dd
yunhua98 <yunhua98@gmail.com>
2016-11-19 23:48:59 -0600

    Generate local map data

--- src/util/GraphLoader.java
+++ src/util/GraphLoader.java
@@ -608,8 +608,8 @@ public class GraphLoader
 		// To use this method to convert your custom map files to custom intersections files
 		// just change YOURFILE in the strings below to be the name of the file you saved.
 		// You can comment out the other method calls above to save time.
-		GraphLoader.createIntersectionsFile("data/maps/YOURFILE.map", 
-					                         "data/intersections/YOURFILE.intersections");
+		GraphLoader.createIntersectionsFile("data/maps/vanderbilt.map", 
+					                         "data/intersections/vanderbilt.intersections");
 
 	}
 	
